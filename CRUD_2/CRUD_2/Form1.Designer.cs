8bd1814d7b196272acf921dfbaad2784f91fd439
Murselov Mirmursel <mmirmursal.p302@code.edu.az>
2017-08-24 16:25:39 +0400

    Create Read Update Delete

---
+++ CRUD_2/CRUD_2/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace CRUD_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxBookName = new System.Windows.Forms.TextBox();
+            this.txtBoxISBN = new System.Windows.Forms.TextBox();
+            this.txtBoxAuthor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnBookAdd = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CMBBOXCatID = new System.Windows.Forms.ComboBox();
+            this.cmBoxCatName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.comboBoxSelectedRow = new System.Windows.Forms.ComboBox();
+            this.btnDeleteRow = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtBoxBookName
+            // 
+            this.txtBoxBookName.Location = new System.Drawing.Point(180, 22);
+            this.txtBoxBookName.Multiline = true;
+            this.txtBoxBookName.Name = "txtBoxBookName";
+            this.txtBoxBookName.Size = new System.Drawing.Size(151, 31);
+            this.txtBoxBookName.TabIndex = 0;
+            // 
+            // txtBoxISBN
+            // 
+            this.txtBoxISBN.Location = new System.Drawing.Point(180, 85);
+            this.txtBoxISBN.Multiline = true;
+            this.txtBoxISBN.Name = "txtBoxISBN";
+            this.txtBoxISBN.Size = new System.Drawing.Size(151, 31);
+            this.txtBoxISBN.TabIndex = 1;
+            // 
+            // txtBoxAuthor
+            // 
+            this.txtBoxAuthor.Location = new System.Drawing.Point(180, 146);
+            this.txtBoxAuthor.Multiline = true;
+            this.txtBoxAuthor.Name = "txtBoxAuthor";
+            this.txtBoxAuthor.Size = new System.Drawing.Size(151, 31);
+            this.txtBoxAuthor.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(6, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "BOOK NAME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(6, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ISBN";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(6, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "AUTHOR";
+            // 
+            // btnBookAdd
+            // 
+            this.btnBookAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnBookAdd.Location = new System.Drawing.Point(106, 257);
+            this.btnBookAdd.Name = "btnBookAdd";
+            this.btnBookAdd.Size = new System.Drawing.Size(93, 30);
+            this.btnBookAdd.TabIndex = 7;
+            this.btnBookAdd.Text = "CREATE";
+            this.btnBookAdd.UseVisualStyleBackColor = true;
+            this.btnBookAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(6, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "CATEGORY_ID";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // CMBBOXCatID
+            // 
+            this.CMBBOXCatID.FormattingEnabled = true;
+            this.CMBBOXCatID.Location = new System.Drawing.Point(180, 217);
+            this.CMBBOXCatID.Name = "CMBBOXCatID";
+            this.CMBBOXCatID.Size = new System.Drawing.Size(151, 21);
+            this.CMBBOXCatID.TabIndex = 10;
+            this.CMBBOXCatID.Click += new System.EventHandler(this.CMBBOXCatID_Click);
+            // 
+            // cmBoxCatName
+            // 
+            this.cmBoxCatName.Location = new System.Drawing.Point(552, 12);
+            this.cmBoxCatName.Multiline = true;
+            this.cmBoxCatName.Name = "cmBoxCatName";
+            this.cmBoxCatName.Size = new System.Drawing.Size(151, 31);
+            this.cmBoxCatName.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(357, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(183, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "CATEGORY CREATE";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(733, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 34);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "CREATE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(376, 58);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(540, 214);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnRead.Location = new System.Drawing.Point(565, 278);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(191, 30);
+            this.btnRead.TabIndex = 13;
+            this.btnRead.Text = "READ vs REFRESH";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // comboBoxSelectedRow
+            // 
+            this.comboBoxSelectedRow.FormattingEnabled = true;
+            this.comboBoxSelectedRow.Location = new System.Drawing.Point(166, 325);
+            this.comboBoxSelectedRow.Name = "comboBoxSelectedRow";
+            this.comboBoxSelectedRow.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxSelectedRow.TabIndex = 14;
+            this.comboBoxSelectedRow.DropDown += new System.EventHandler(this.comboBoxSelectedRow_DropDown);
+            this.comboBoxSelectedRow.SelectedIndexChanged += new System.EventHandler(this.comboBoxSelectedRow_SelectedIndexChanged);
+            this.comboBoxSelectedRow.SelectedValueChanged += new System.EventHandler(this.comboBoxSelectedRow_SelectedValueChanged);
+            // 
+            // btnDeleteRow
+            // 
+            this.btnDeleteRow.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDeleteRow.Location = new System.Drawing.Point(12, 379);
+            this.btnDeleteRow.Name = "btnDeleteRow";
+            this.btnDeleteRow.Size = new System.Drawing.Size(169, 30);
+            this.btnDeleteRow.TabIndex = 15;
+            this.btnDeleteRow.Text = "DELETE ROW";
+            this.btnDeleteRow.UseVisualStyleBackColor = true;
+            this.btnDeleteRow.Click += new System.EventHandler(this.btnDeleteRow_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(217, 379);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 30);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "UPDATE ROW";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(21, 326);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 20);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "CHOOSE ROW";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(992, 556);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnDeleteRow);
+            this.Controls.Add(this.comboBoxSelectedRow);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CMBBOXCatID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnBookAdd);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmBoxCatName);
+            this.Controls.Add(this.txtBoxAuthor);
+            this.Controls.Add(this.txtBoxISBN);
+            this.Controls.Add(this.txtBoxBookName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxBookName;
+        private System.Windows.Forms.TextBox txtBoxISBN;
+        private System.Windows.Forms.TextBox txtBoxAuthor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnBookAdd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox CMBBOXCatID;
+        private System.Windows.Forms.TextBox cmBoxCatName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.ComboBox comboBoxSelectedRow;
+        private System.Windows.Forms.Button btnDeleteRow;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CRUD_2/CRUD_2/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD_2
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+        CRUD_2Entities db = new CRUD_2Entities();
+        BooksTable BookTable1 = new BooksTable();
+        
+    private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            string BookNameData = txtBoxBookName.Text;
+            string ISBNData = txtBoxISBN.Text;
+            string AuthorData = txtBoxAuthor.Text;
+            string CategoryData = CMBBOXCatID.Text;
+            CMBBOXCatID.Text = "";
+         
+
+
+
+            if (string.IsNullOrEmpty(BookNameData) || string.IsNullOrEmpty(ISBNData) || string.IsNullOrEmpty(AuthorData))
+            {
+                MessageBox.Show("Bos Buraxma!!!");
+            }
+            else
+            {
+                BookTable1.BookName = BookNameData;
+                BookTable1.ISBN = Convert.ToInt32(ISBNData);
+                BookTable1.Author = AuthorData;
+                BookTable1.CategoryID = db.CategoryTables.Where(x=> x.CategoryName == CMBBOXCatID.SelectedItem.ToString()).First().id;
+                db.BooksTables.Add(BookTable1);
+
+
+                
+                db.SaveChanges();
+                txtBoxAuthor.Clear();
+                txtBoxBookName.Clear();
+                txtBoxISBN.Clear();
+      
+
+            }
+          
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            CategoryTable CategoryTable1 = new CategoryTable();
+            CategoryTable1.CategoryName = cmBoxCatName.Text;
+            cmBoxCatName.Clear();
+            db.CategoryTables.Add(CategoryTable1);
+            db.SaveChanges();
+
+        }
+
+        private void CMBBOXCatID_Click(object sender, EventArgs e)
+        {
+            CMBBOXCatID.Items.Clear();
+            foreach (var item in db.CategoryTables.ToList())
+            {
+                CMBBOXCatID.Items.Add(item.CategoryName);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.BooksTables.ToList();
+        }
+
+        private void btnDeleteRow_Click(object sender, EventArgs e)
+        {
+            //BookTable1.CategoryID = db.CategoryTables.Where(x => x.CategoryName == CMBBOXCatID.SelectedItem.ToString()).First().id;
+           int setir = Convert.ToInt32(comboBoxSelectedRow.SelectedItem);
+            foreach (var item in db.BooksTables)
+            {
+                if (item.id==setir)
+                {
+                    db.BooksTables.Remove(item);
+                    comboBoxSelectedRow.Items.Remove(comboBoxSelectedRow.SelectedItem);
+                    comboBoxSelectedRow.Text = "";
+                    
+                }
+            }
+            dataGridView1.DataSource = db.BooksTables.ToList();
+            db.SaveChanges();
+        }
+        //Uptade begin
+        private void button2_Click(object sender, EventArgs e)
+        {
+            BooksTable booktable2 = new BooksTable();
+            string bookData1 = txtBoxBookName.Text;
+            int ISBNdata1 = Convert.ToInt32(txtBoxISBN.Text);
+            string authorDAta1 = txtBoxAuthor.Text;
+            string CategoryData1 = CMBBOXCatID.Text;
+
+            int setirUpdated = Convert.ToInt32(comboBoxSelectedRow.SelectedItem);
+            foreach (var item in db.BooksTables)
+            {
+                
+                if (item.id == setirUpdated)
+                {
+                    //db.BooksTables.;
+                    //comboBoxSelectedRow.Items.Remove(comboBoxSelectedRow.SelectedItem);
+                    //comboBoxSelectedRow.Text = "";
+                    
+                    item.BookName = bookData1;
+                    item.ISBN = ISBNdata1;
+                    item.Author = authorDAta1;
+                    item.CategoryID = db.CategoryTables.Where(x => x.CategoryName == CMBBOXCatID.SelectedItem.ToString()).First().id;
+
+                    txtBoxAuthor.Clear();
+                    txtBoxBookName.Clear();
+                    txtBoxISBN.Clear();
+                    comboBoxSelectedRow.Text = "";
+                    CMBBOXCatID.Text = "";
+
+                   
+                }
+            }
+          
+            db.SaveChanges();
+        }
+
+        private void comboBoxSelectedRow_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+    }
+
+        private void comboBoxSelectedRow_SelectedValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxSelectedRow_DropDown(object sender, EventArgs e)
+        {
+            comboBoxSelectedRow.Items.Clear();
+            var elements = db.BooksTables.ToList();
+            foreach (var item in elements)
+            {
+                comboBoxSelectedRow.Items.Add(item.id);
+            }
+        }
+
+        //UPDATE END
+    }
+}
